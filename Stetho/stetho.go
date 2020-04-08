5f67a33e8056bfd9d7ebe270d2d2f96b68ee8795
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2020-04-09 05:01:13 +0800

    custom client + periodic get req with timeout

--- Stetho/stetho.go
+++ Stetho/stetho.go
@@ -1,76 +1,105 @@
 package Stetho
 
 import (
-	"50.041-DistSysProject-BunshinDB/ConHash"
+	"50.041-DistSysProject-BunshinDB/pkg/ConHash"
+	"50.041-DistSysProject-BunshinDB/pkg/ConHttp"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
-type Stetho struct {
+type StethoNode struct {
 
+	client http.Client
 	nodes []ConHash.Node
-	ringServer Ring
-	port int
+	ringServer ConHttp.Ring
+	port string
+	pingIntervalSeconds int //How long the stethoscope should wait after each cycle?
 }
 
-func (s *Stetho) addNode(n *ConHash.Node){
-	s.nodes = append(s.nodes, *n)
+func (s *StethoNode) AddNode(n ConHash.Node){
+	s.nodes = append(s.nodes, n)
 
 }
 
-func (s *Stetho) addRing(r *Ring){
-	s.ringServer = *r
+func (s *StethoNode) SetRing(r ConHttp.Ring){
+	s.ringServer = r
 }
 
-
-func (s *Stetho) listen(){
+//TODO: Can explore making listen() async. Ping shall be synchronous for now.
+func (s *StethoNode) listen(){
+	time.Sleep(time.Duration(5 * time.Second))
 	log.Print("listening")
-	//for {
-	//	for _, node := range(s.nodes){
-	//		fmt.Println(node.IP)
-	//	}
-	//	fmt.Println("lala")
-	//}
+	for {
+		for _, node := range(s.nodes){
+			//https://github.com/golang/go/issues/18824
+			urlString := fmt.Sprintf("http://%s:%s/%s", node.IP, node.Port, "hb")
+			log.Print(fmt.Sprintf("Pinging %s at %s",
+				node.CName, urlString))
+
+			resp, err := s.client.Get(urlString)
+
+			//Fails for some reason
+			//TODO: need to be able to differentiate the type of failure such as timeout vs no host vs invalid port etc.
+			if err != nil {
+
+
+			}
+
+			log.Println(resp.StatusCode)
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			log.Println(string(body))
+		}
+		time.Sleep(time.Duration(1 * time.Second))
+	}
 }
 
-func (s *Stetho) HttpServerStart(){
+func (s *StethoNode) HttpServerStart(){
 
 	http.HandleFunc("/set-ring", s.SetRingHandler)
 	http.HandleFunc("/add-node", s.AddNodeHandler)
-	log.Print(fmt.Sprintf("Stetho Node listening at %s.",externalIP()))
+	ip, err := ConHttp.ExternalIP()
+
+	if err == nil {
+		log.Print(fmt.Sprintf("StethoNode Node listening at %s.", ip ))
+	} else {
+		log.Print(err)
+	}
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",s.port), nil))
 }
 
-func (s *Stetho) SetRingHandler(w http.ResponseWriter, r *http.Request) {
+func (s *StethoNode) SetRingHandler(w http.ResponseWriter, r *http.Request) {
 	//To-Do update ring
 	//Need a onUpdateRing function in conHash.go
 	log.Print("set-ring")
 }
 
-func (s *Stetho) AddNodeHandler(w http.ResponseWriter, r *http.Request) {
+func (s *StethoNode) AddNodeHandler(w http.ResponseWriter, r *http.Request) {
 	//To-Do update ring
 	//Need a onUpdateRing function in conHash.go
 	log.Print("add-node")
 }
 
-func (s *Stetho) start(){
-	s.HttpServerStart()
+func (s *StethoNode) Start(){
 	s.listen()
-}
-
-func NewStetho(port int) Stetho{
-	nodes := []ConHash.Node {}
-	return Stetho{nodes, nil, port}
-}
-
-
-func main() {
-
-	var s Stetho = NewStetho(5000)
-	s.start()
+	s.HttpServerStart()
 
 }
+
+func NewStetho(port string, numSeconds int, timeoutSeconds int) StethoNode {
+	client := http.Client{Timeout:time.Duration(time.Duration(timeoutSeconds) * time.Second)}
+	nodes := []ConHash.Node{}
+	dummyRing := ConHttp.Ring{}
+	return StethoNode{client, nodes, dummyRing, port, numSeconds}
+}
+
 
 
