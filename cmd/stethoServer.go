abba938eccd55d712bd7038e83505d2ccaf34e3b
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2020-04-24 15:33:56 +0800

    command files

--- cmd/stethoServer.go
+++ cmd/stethoServer.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"50.041-DistSysProject-BunshinDB/pkg/ServerUtils"
+	"fmt"
+)
+
+func main() {
+	const PING_INTERVAL = 1
+	const TIMEOUT_INTERVAL = 5
+
+	fmt.Println("Stetho running...")
+	s:= ServerUtils.NewStethoServer("5000", PING_INTERVAL, TIMEOUT_INTERVAL)
+	s.Start()
+}
